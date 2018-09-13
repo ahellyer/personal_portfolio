7a4ff6262ca7d7a2c589cd7f1da5fa7e224009e7
Ashley <ashleyhellyer@gmail.com>
2018-09-13 16:05:54 -0400

    added portfolio screenshots and styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,13 @@
+import AOS from 'aos';
+
 const hello = 'World';
 console.log(hello);
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log(AOS);
+    AOS.init();
+});
+
+
+
+
